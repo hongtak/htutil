3766b1787b14b3b6ce27f830d029d90e726ec8b5
Choi Hong Tak <hongtak@gmail.com>
2020-09-03 14:37:24 +0800

    rewrite redis lib

--- redislib.js
+++ redislib.js
@@ -15,36 +15,39 @@ promisifyAll(redis.RedisClient.prototype)
 promisifyAll(redis.Multi.prototype)
 
 class RedisClient {
-  static connect (config) {
+  config (config) {
+    this._config = config
+  }
+
+  connect () {
     if (RedisClient._client) {
       RedisClient._client.quit()
     }
-    RedisClient._client = redis.createClient(`redis://${config.server}`)
+    RedisClient._client = redis.createClient(`redis://${this._config.server}`)
     RedisClient._client.on('connect', () => {
-      logger.info('RedisLib> Redis connected')
+      logger.info('RedisLib -> Redis connected')
     })
 
     RedisClient._client.on('ready', () => {
-      logger.info(`RedisLib> Redis ready (${RedisClient._client.server_info.redis_version})`)
+      logger.info(`RedisLib -> Redis ready (${RedisClient._client.server_info.redis_version})`)
     })
 
     RedisClient._client.on('reconnecting', () => {
-      logger.info('RedisLib> Redis reconnecting')
+      logger.info('RedisLib -> Redis reconnecting')
     })
 
     RedisClient._client.on('error', (err) => {
-      logger.error('RedisLib> Redis error: ' + err)
+      logger.error('RedisLib -> Redis error: ' + err)
     })
 
     RedisClient._client.on('end', () => {
-      logger.info('RedisLib> Redis disconnected')
+      logger.info('RedisLib -> Redis disconnected')
     })
-    return RedisClient._client
   }
 
-  static sharedClient () {
+  get client () {
     return RedisClient._client
   }
 }
 
-module.exports = RedisClient
+module.exports = new RedisClient()
