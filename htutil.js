766722086247ac103442d6b6aacd1d2ee91647b6
Choi Hong Tak <hongtak@gmail.com>
2020-10-03 08:54:23 +0800

    removed old logger and v1.1.0

--- htutil.js
+++ htutil.js
@@ -1,11 +1,9 @@
-const logger = require('./logger')
 const MongoDb = require('./mongolib')
 const uuid = require('./uuid')
 const RedisClient = require('./redislib')
 const htcrypto = require('./htcrypto')
 const loggerlib = require('./loggerlib')
 module.exports = {
-  logger,
   MongoDb,
   uuid,
   RedisClient,
